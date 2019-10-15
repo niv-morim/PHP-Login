de763065c31fe2dbc4569ab49cf975b1270d0ad7
Nivmorim <nivi235@gmail.com>
2019-10-15 19:12:59 +0300

    connect to DB

---
+++ inc/classes/DB.php
@@ -0,0 +1,37 @@
+<?php
+if(!defined('_config_')){
+    exit('you dont have a config file');
+}
+class DB {
+
+    protected static $con;
+    
+    // $charset="utf8_general_ci";
+    // $servername = "localhost";
+    // $myDB="login_system"
+    // $username = "root";
+    // $password = "";
+
+private function __construct() {
+
+    try {
+        $conn = new PDO("mysql:charset=utf8;host=localhost;dbname=login_system", "root", "");
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+        }
+    catch(PDOException $e)
+        {
+        echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+public static function getConnection(){
+    if(!self::$con)
+    {
+        new DB();
+    }
+    return self::$con;
+}
+}
+?>
--- inc/config.php
+++ inc/config.php
@@ -2,4 +2,10 @@
 if(!defined('_config_')){
     exit('you dont have a config file');
 }
+
+//include the DB.php file:
+include_once "classes/DB.php";
+
+$con=DB::getConnection();
+
 ?>

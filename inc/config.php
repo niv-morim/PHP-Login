2fdc1cc5d726c04cf9d140298a8cc7275fa3e844
Nivmorim <nivi235@gmail.com>
2019-10-15 13:26:14 +0300

    config page

---
+++ inc/config.php
@@ -0,0 +1,5 @@
+<?php
+if(!defined('_config_')){
+    exit('you dont have a config file');
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+//allow the confog
+define('_config_', true);
+//require the config
+require_once "inc/config.php"; ?>
+
 <!DOCTYPE html>
 
     <head>
@@ -36,7 +42,7 @@
     </div>
     </div>
 
-
+    <?php require_once "inc/footer.php"; ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <!-- UIkit JS -->
